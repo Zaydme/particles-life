b29418e722aa2bdd7a7d0049a78104b3d43fa197
Zaydme <Zaydme8@gmail.com>
2020-06-03 21:28:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Particles Life
+Particles Life (or Game of life with particles)
+![Particles life example](https://i.imgur.com/cgcWa92.png)
+Particles apply forces on each others. Each color applies a predefined attraction/repulsion force on every other color inside a predefined radius.
+*The attraction/repulsion forces are different every time you run the script.*
+
+There is a basic mode, that has only 2 colors, red and blue, red attracts red and repulses blue, blue attracts blue and repulses red. To turn on basic mode click `toggle basic mode`
+or make your own preset
+
+Based on [Particle-Life](https://github.com/HackerPoet/Particle-Life "Go to the repository of Particle-Life") by [HackerPoet](https://github.com/HackerPoet "Go to HackerPoet's GitHub profile").
---
+++ sketch.js
@@ -0,0 +1,201 @@
+document.body.style.margin = 0
+const width = document.body.clientWidth
+const height = document.body.clientHeight
+
+let particlesCount = 100;
+let particles = [];
+
+
+random = p5.prototype.random
+
+let colors = {
+    red: [255, 0, 0],
+    green: [0, 255, 0],
+    blue: [0, 0, 255],
+    yellow: [255, 255, 0]
+}
+
+let presets = [{ color: 'red', pref: { red: random(-1, 1), blue: random(-1, 1), yellow: random(-1, 1), green: random(-1, 1) } },
+{ color: 'blue', pref: { red: random(-1, 1), blue: random(-1, 1), yellow: random(-1, 1), green: random(-1, 1) } },
+{ color: 'yellow', pref: { red: random(-1, 1), blue: random(-1, 1), yellow: random(-1, 1), green: random(-1, 1) } },
+{ color: 'green', pref: { red: random(-1, 1), blue: random(-1, 1), yellow: random(-1, 1), green: random(-1, 1) } }]
+
+
+// basic testing preset where every color likes itself and hates the other color
+basicPresets = [{ color: 'red', pref: { red: 1, blue: -1,  } },
+                          { color: 'blue', pref: { red: -1, blue: 1,  } }]
+
+
+function setup() {
+  createCanvas(width, height);
+
+  for (let i = 0; i < particlesCount; i++) {
+    let pres = presets[Math.floor(Math.random() * presets.length)]
+    particles[i] = new Particle(random(width), random(height), pres.color,pres.pref);
+  }
+}
+
+function draw() {
+  background(10);
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].live(particles);
+  }
+  noStroke(255)
+  fill(255);
+  text(`${particles.length} particles, fps: ${frameRate().toFixed()}`, 20, 20)
+}
+
+
+
+class Particle {
+  constructor(x, y, color, pref) {
+    this.acceleration = createVector(0, 0);
+    this.velocity = p5.Vector.random2D();
+    this.position = createVector(x, y);
+    this.r = 3.0;
+    this.maxspeed = 3;
+    this.maxforce = 0.05;
+    this.color = color
+    this.pref = pref
+  }
+
+  live(prtcls) {
+    this.regroup(prtcls);
+    this.update();
+    this.borders();
+    this.render();
+  }
+  
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+  
+  regroup(prtcls) {
+    let sep = this.separate(prtcls);
+    let ali = this.align(prtcls);
+    let coh = this.cohesion(prtcls);
+
+    sep.mult(2.5);
+    ali.mult(1.3);
+    coh.mult(1.0);
+
+    this.applyForce(sep);
+    this.applyForce(ali);
+    this.applyForce(coh);
+  }
+  
+
+  update() {
+
+    this.velocity.add(this.acceleration);
+
+    this.velocity.limit(this.maxspeed);
+    this.position.add(this.velocity);
+
+    this.acceleration.mult(0);
+  }
+  
+
+  seek(target) {
+    let desired = p5.Vector.sub(target, this.position);
+
+    desired.normalize();
+    desired.mult(this.maxspeed);
+    
+    let steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxforce); 
+    return steer;
+  }
+  
+  render() {
+    fill(colors[this.color][0],colors[this.color][1],colors[this.color][2], 100);
+    stroke(this.color);
+    ellipse(this.position.x, this.position.y, 16, 16);
+  }
+  
+  borders() {
+    if (this.position.x < -this.r) this.position.x = width + this.r;
+    if (this.position.y < -this.r) this.position.y = height + this.r;
+    if (this.position.x > width + this.r) this.position.x = -this.r;
+    if (this.position.y > height + this.r) this.position.y = -this.r;
+  }
+  
+  separate(prtcls) {
+    let desiredseparation = 25.0;
+    let steer = createVector(0, 0);
+    let count = 0;
+    for (let i = 0; i < prtcls.length; i++) {
+      let d = p5.Vector.dist(this.position, prtcls[i].position);
+      if ((d > 0) && (d < desiredseparation)) {
+        let diff = p5.Vector.sub(this.position, prtcls[i].position);
+        diff.normalize();
+        diff.div(d);
+        steer.add(diff);
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      steer.div(count);
+    }
+  
+    if (steer.mag() > 0) {
+      steer.normalize();
+      steer.mult(this.maxspeed);
+      steer.sub(this.velocity);
+      steer.limit(this.maxforce);
+    }
+    return steer;
+  }
+  
+  align(prtcls) {
+    let neighbordist = 50;
+    let sum = createVector(0, 0);
+    let count = 0;
+    for (let i = 0; i < prtcls.length; i++) {
+      let d = p5.Vector.dist(this.position, prtcls[i].position);
+      if ((d > 0) && (d < neighbordist)) {
+        // important part
+        sum.add(createVector(prtcls[i].velocity.x*this.pref[prtcls[i].color],prtcls[i].velocity.y*this.pref[prtcls[i].color]))
+        count++;
+      }
+    }
+    if (count > 0) {
+      sum.div(count);
+      sum.normalize();
+      sum.mult(this.maxspeed);
+      let steer = p5.Vector.sub(sum, this.velocity);
+      steer.limit(this.maxforce);
+      return steer;
+    } else {
+      return createVector(0, 0);
+    }
+  }
+  
+  cohesion(prtcls) {
+    let neighbordist = 50;
+    let sum = createVector(0, 0);
+    let count = 0;
+    for (let i = 0; i < prtcls.length; i++) {
+      let d = p5.Vector.dist(this.position, prtcls[i].position);
+      if ((d > 0) && (d < neighbordist)) {
+        sum.add(prtcls[i].position);
+        count++;
+      }
+    }
+    if (count > 0) {
+      sum.div(count);
+      return this.seek(sum);
+    } else {
+      return createVector(0, 0);
+    }
+  }  
+}
+
+function toggleBasicMode() {
+    if (presets == basicPresets) location.reload()
+    presets = basicPresets
+    setup()
+}
